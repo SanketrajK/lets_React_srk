141cdc4cbb0bed4f89c4df09d8b2ef685b5a29b8
SanketrajK <sanketrajsrk16@gmail.com>
2024-09-21 10:58:20 +0530

    episode 03

---
+++ README.md
@@ -0,0 +1,20 @@
+# lets react
+
+# Parcel
+
+- Dev Build
+- Local Server
+- HMR = Hot Module Replacement (as we make change in code and save the file it automatically update it in the browser)
+- Using File Watching Algorithm - written in C++
+- Caching - Faster Builds
+- Image Optimization
+- Minification
+- Bundling
+- Compress files
+- Consistent Hashing
+- Code Spliting
+- Differential Bundling
+- Diagonisgtic
+- Error Handling
+- Tree Shakeing - remove unused code
+- Diiferent dev and prod build
--- app.js
+++ app.js
@@ -1,5 +1,28 @@
-const heading = React.createElement("h1", {}, "Hello World from React!");
+import React from "react";
+import ReactDOM from "react-dom/client";
+//#React Element----------------------------------------------------------------
+//JSX - HTML like - XML like syntax
+//JSX - (transpiled beforev it reaches the JS) - PARCEL - BABEL
 
-const root = ReactDom.createRoot(document.getElementById("root"));
+//-------------------------------------------------------------------------------
+const title = () => (
+  <h1 className="head" tabIndex="5">
+    Namaste React👀 Using JSX😎
+  </h1>
+);
 
-root.render(heading);
+//#React Functional  Component
+
+const data = 1000;
+
+const HeadingComponent = () => (
+  <div id="container">
+    {title()}
+    <h2>{console.log("kya bolti public")} </h2>
+    <h1 className="heading">Namaste React Functional Component👌</h1>
+  </div>
+);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(<HeadingComponent />);
